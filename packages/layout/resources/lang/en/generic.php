cde88b39fc7962a0c177507a46ce34b8378adcb8
Ben <howdu@users.noreply.github.com>
2025-11-21 07:40:02 +0000

    Update HasAssetsRelationManager.php

--- packages/layout/resources/lang/en/generic.php
+++ packages/layout/resources/lang/en/generic.php
@@ -3,7 +3,18 @@
 declare(strict_types=1);
 
 return [
-    'container_settings' => 'Container Settings',
+    'breadcrumbs' => 'Breadcrumbs',
+    'page_children' => 'Page Children',
+    'contents' => 'Contents',
+    'gallery' => 'Gallery',
+    'latest_pages' => 'Latest Pages',
+    'media_carousel' => 'Media Carousel',
+    'page_content' => 'Page Content',
+    'pages_card' => 'Pages Card',
+    'page_slot' => 'Page Slot',
     'pages_tile' => 'Pages',
+    'faq' => 'FAQ',
+    'copy_page_layout' => 'Copy this layout? :link',
     'fixed_content_tree' => 'Content tree fixed successfully.',
 ];
+
