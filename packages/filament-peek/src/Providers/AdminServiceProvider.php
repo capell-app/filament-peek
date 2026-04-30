55253ed6f037d9083e1c75ba141380fa8406b330
Ben <howdu@users.noreply.github.com>
2026-04-30 23:37:07 +0100

    feat(filament-peek): add workspace modal preview

--- packages/filament-peek/src/Providers/AdminServiceProvider.php
+++ packages/filament-peek/src/Providers/AdminServiceProvider.php
@@ -6,6 +6,8 @@ namespace Capell\FilamentPeek\Providers;
 
 use Capell\Admin\Contracts\Extenders\AdminPanelExtender;
 use Capell\FilamentPeek\Filament\Extenders\FilamentPeekAdminPanelExtender;
+use Capell\FilamentPeek\Workspaces\WorkspacePeekPreviewActionContributor;
+use Capell\Workspaces\Contracts\WorkspaceTableActionContributor;
 use Illuminate\Support\ServiceProvider;
 
 final class AdminServiceProvider extends ServiceProvider
@@ -13,5 +15,6 @@ final class AdminServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->tag([FilamentPeekAdminPanelExtender::class], AdminPanelExtender::TAG);
+        $this->app->tag([WorkspacePeekPreviewActionContributor::class], WorkspaceTableActionContributor::TAG);
     }
 }
