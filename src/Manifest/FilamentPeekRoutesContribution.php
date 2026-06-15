cbee3a9492a7035928bb6a8756b02bb143ca7ef2
Ben <howdu@users.noreply.github.com>
2026-06-15 12:04:40 +0100

    fix: expose filament peek route metadata

---
+++ src/Manifest/FilamentPeekRoutesContribution.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\FilamentPeek\Manifest;
+
+use Capell\Core\Contracts\Extensions\ExtensionContribution;
+use Capell\Core\Contracts\Extensions\RegistersExtensionRoute;
+
+final class FilamentPeekRoutesContribution implements ExtensionContribution, RegistersExtensionRoute
+{
+    public static function compatibleCapellApiVersion(): string
+    {
+        return '^4.0';
+    }
+}
