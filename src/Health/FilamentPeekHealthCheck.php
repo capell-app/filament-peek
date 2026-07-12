a2ac755234565e005d7045687fd441745b104ba7
Ben <howdu@users.noreply.github.com>
2026-07-12 09:31:33 +0100

    chore: harden package portfolio for go-live

--- src/Health/FilamentPeekHealthCheck.php
+++ src/Health/FilamentPeekHealthCheck.php
@@ -33,7 +33,7 @@ final class FilamentPeekHealthCheck implements ChecksExtensionHealth
 
     public static function compatibleCapellApiVersion(): string
     {
-        return '^0.0';
+        return '^4.0';
     }
 
     /**
